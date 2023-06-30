b38bb56bd7bd03c8261d1e4e3a08ac1eadfd6782
jterkuc <jterkuc@gmail.com>
2023-06-30 15:50:59 -0400

    Fixed character creation bug

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 # Made with <3 by Jacob Terkuc
 
-import openai, os, ast
+import openai, os, ast, string, random
 import settings, character
 from dotenv import load_dotenv
 
@@ -25,18 +25,17 @@ def generate(msg):
 
 # Function that takes an integer 'num', and an optional string 'desc' and returns a list of 'num' character.
 # If 'desc' is not provided, it will default to creating 'num' random character of type 'race'.
-def create_character(num, desc=""):
+def create_character(num=1, desc=""):
     print("Creating character...")
     c_list = []
-    c_calls = []
 
     if desc == "":
-        prompt = "You are tasked to make up " + str(num) + " character in a " + settings.game_style + \
-                 "-style game. " \
+        prompt = "You are tasked to make up " + str(num) + " character(s) in a " + settings.game_style + \
+                 "-style game. It is crucial that you do not make more than " + str(num) + " character(s)." \
                  "Output your responses as a list of python lists, the length of the list being the number of " \
-                 "character needing to be created, and each of the nested lists separated with '\n\n', and laid out " \
+                 "character(s) needing to be created, and each of the nested lists separated with '\n\n', and laid out " \
                  "like: " + str(character.character_details) + \
-                 ". Make sure none of the characters you create identical, and that they are all unique. An " \
+                 ". Make sure none of the character() you create identical, and that they are all unique. An " \
                  "example of a character that you could create is: ['John', 'John is a farmer who lives in Centretown. " \
                  "He loves nature, and frequently likes going on walks through nature. He is kind and responds to other " \
                  "characters in a kind and relaxed tone. ' 'Farmer', 100, 10, 10, [], 'Centretown']. As additional " \
@@ -47,14 +46,27 @@ def create_character(num, desc=""):
                  "and can be anything, as long as it details at least 100 words about the character's personality. For " \
                  "the LOCATION, choose a location that would fit the ROLE that the entity has (ex. A farmer would be " \
                  "located in a more rural area).You can pick from the following cities/zones: " + \
-                 str(settings.default_location)
+                 str(settings.default_location) + ". Additionally, DO NOT USE THE ''' CHARACTER IN YOUR RESPONSES. " \
 
         x = ast.literal_eval(generate([{"role": "system", "content": prompt}])["choices"][0]['message']['content'])
 
     else:
-        x = desc
+        x = ast.literal_eval(generate([{"role": "system", "content": desc}])["choices"][0]['message']['content'])
 
-    for i in range(len(x)):
-        c_list.append(character.Character(x[i][0], x[i][1], x[i][2], x[i][3], x[i][4], x[i][5], x[i][6], x[i][7]))
+    print(x)
 
-    return c_list
+    if num > 1:
+        for i in range(len(x)):
+            c_list.append(character.Character(x[i][0], x[i][1], x[i][2], x[i][3], x[i][4], x[i][5], x[i][6], x[i][7]))
+
+    else:
+        i = 0
+        return character.Character(x[i][0], x[i][1], x[i][2], x[i][3], x[i][4], x[i][5], x[i][6], x[i][7])
+
+
+# Function that generates a random 8-digit 'id' string and returns it.
+def generate_id(length=8):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join(random.choice(letters_and_digits) for i in range(length))
+    return result_str
+
